138a4a1eae68c8ba4b2a9acea45c2784daf2cf83
gonza <gonza@DESKTOP-B9U4ILK>
2021-12-24 19:08:14 +0100

    Interfaz AppVideo

---
+++ MavenProject/src/main/java/umu/tds/interfaz/AppVideo.java
@@ -0,0 +1,43 @@
+package umu.tds.interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AppVideo {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AppVideo window = new AppVideo();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AppVideo() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
