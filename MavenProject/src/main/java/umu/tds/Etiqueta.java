05ebb332491d81421b5203791484a050682a91ae
Roberto <robelmontem@gmail.com>
2021-12-24 17:43:30 +0100

    Creación Clase Etiqueta

---
+++ MavenProject/src/main/java/umu/tds/Etiqueta.java
@@ -0,0 +1,9 @@
+package umu.tds;
+
+public class Etiqueta {
+	private String nombre;
+	
+	public String getNombre() {
+		return nombre;
+	}
+}
--- MavenProject/src/main/java/umu/tds/Video.java
+++ MavenProject/src/main/java/umu/tds/Video.java
@@ -1,16 +1,23 @@
 package umu.tds;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Video {
 
 	private String url;
 	private String titulo;
 	private int numRepro;
-	
+	private Set<Etiqueta> etiquetas;
 
-	public Video(String url, String titulo) {
+	public Video(String url, String titulo, Etiqueta... etiquetas) {
 		this.url = url;
 		this.titulo = titulo;
 		this.numRepro = 0;
+		this.etiquetas = new HashSet<Etiqueta>();
+		for (Etiqueta etiq : etiquetas) {
+			this.etiquetas.add(etiq);
+		}
 	}
 
 	public int getNumRepro() {
