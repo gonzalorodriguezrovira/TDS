41bfb20026ff81097f9a3f9a84ee2bf36515fa7d
Roberto <robelmontem@gmail.com>
2021-12-26 18:08:06 +0100

    Ajuste tam pantalla

--- MavenProject/src/main/java/umu/tds/nuevoInterfaz/Explorar.java
+++ MavenProject/src/main/java/umu/tds/nuevoInterfaz/Explorar.java
@@ -6,6 +6,20 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import com.toedter.calendar.JDateChooser;
+import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.Box;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class Explorar extends JPanel {
 
@@ -13,15 +27,24 @@ public class Explorar extends JPanel {
 	 * Create the panel.
 	 */
 	public Explorar() {
-		JButton bCancelarRegistro = new JButton("Cancelar");
-		add(bCancelarRegistro);
+		setLayout(null);
 		
-		bCancelarRegistro.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				CardLayout c = (CardLayout)(AppVideo.vDisplay.getLayout());
-				c.show(AppVideo.vDisplay, AppVideo.BRUH);
-			}
-		});
+		JPanel panel = new JPanel();
+		panel.setBounds(10, 11, 463, 367);
+		add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.add(panel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.add(panel_2);
+		
+		JPanel pEtiquetas = new JPanel();
+		pEtiquetas.setBounds(483, 17, 34, 314);
+		pEtiquetas.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		add(pEtiquetas);
 	}
-
 }
