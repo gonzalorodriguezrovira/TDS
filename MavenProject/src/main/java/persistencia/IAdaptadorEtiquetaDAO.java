9c5eb18be33cb9e8a9fd82161cfad1e626cb8030
Roberto <robelmontem@gmail.com>
2022-01-02 18:36:36 +0100

    Interfaz Etiqueta

--- MavenProject/src/main/java/persistencia/IAdaptadorEtiquetaDAO.java
+++ MavenProject/src/main/java/persistencia/IAdaptadorEtiquetaDAO.java
@@ -1,5 +1,13 @@
 package persistencia;
 
-public interface IAdaptadorEtiquetaDAO {
+import java.util.List;
 
+import modelo.Etiqueta;
+
+public interface IAdaptadorEtiquetaDAO {
+	public void addEtiqueta(Etiqueta lv);
+	public void borrarEtiqueta(Etiqueta lv);
+	public Etiqueta recuperarEtiqueta(int codigo);
+	public List<Etiqueta> recuperarEtiqueta();
+	public void modificarEtiqueta(Etiqueta lv);
 }
